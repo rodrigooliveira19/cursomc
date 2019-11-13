fac394e4fa95d3e986867bb15413e017e3bbfef5
rodrigooliveira19 <oliveirarodrigo19@yahoo.com.br>
2019-11-13 11:34:59 -0300

    Criando repository e service para categoria

--- src/main/java/com/nelioalves/cursomc/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/cursomc/resources/CategoriaResource.java
@@ -3,27 +3,29 @@ package com.nelioalves.cursomc.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nelioalves.cursomc.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.cursomc.services.CategoriaService;
 
-@RestController //Define a classe como um Controlador Rest
+//@RestController Define a classe como um Controlador Rest
+@RestController 
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria> listar() {
+	@Autowired
+	private CategoriaService service; 
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
-		Categoria cat1 = new Categoria(1, "INFORMATICA"); 
-		Categoria cat2 = new Categoria(2, "ESCRITORIO"); 
-		
-		List<Categoria> lista = new ArrayList(); 
-		lista.add(cat1); 
-		lista.add(cat2); 
-		
-		return lista; 
+		Categoria obj = service.buscar(id); 
+		return ResponseEntity.ok().body(obj); 
 	}
 
 }
---
+++ src/main/java/com/nelioalves/cursomc/cursomc/services/CategoriaService.java
@@ -0,0 +1,24 @@
+package com.nelioalves.cursomc.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.cursomc.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	//@Autowired instancia um obj automaticamente pelo spring. 
+	@Autowired
+	private CategoriaRepository repo; 
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id); 
+		return obj.orElse(null); 
+		
+	}
+
+}
